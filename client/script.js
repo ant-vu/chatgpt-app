cfe9997bd246061a913a8c6d0dca84215cc26dfe
Anthony Vu <vua11@mcmaster.ca>
2023-01-01 21:10:50 -0500

    clean up code

--- client/script.js
+++ client/script.js
@@ -10,14 +10,12 @@ function loader(element) {
     element.textContent = ''
 
     loadInterval = setInterval(() => {
-        // Update the text content of the loading indicator
-        element.textContent += '.';
+        element.textContent += '.'
 
-        // If the loading indicator has reached three dots, reset it
         if (element.textContent === '....') {
-            element.textContent = '';
+            element.textContent = ''
         }
-    }, 300);
+    }, 300)
 }
 
 function typeText(element, text) {
@@ -33,15 +31,12 @@ function typeText(element, text) {
     }, 20)
 }
 
-// generate unique ID for each message div of bot
-// necessary for typing text effect for that specific reply
-// without unique ID, typing text will work on every element
 function generateUniqueId() {
-    const timestamp = Date.now();
-    const randomNumber = Math.random();
-    const hexadecimalString = randomNumber.toString(16);
+    const timestamp = Date.now()
+    const randomNumber = Math.random()
+    const hexadecimalString = randomNumber.toString(16)
 
-    return `id-${timestamp}-${hexadecimalString}`;
+    return `id-${timestamp}-${hexadecimalString}`
 }
 
 function chatStripe(isAi, value, uniqueId) {
@@ -67,23 +62,17 @@ const handleSubmit = async (e) => {
 
     const data = new FormData(form)
 
-    // user's chatstripe
     chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
 
-    // to clear the textarea input 
     form.reset()
 
-    // bot's chatstripe
     const uniqueId = generateUniqueId()
     chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
 
-    // to focus scroll to the bottom 
-    chatContainer.scrollTop = chatContainer.scrollHeight;
+    chatContainer.scrollTop = chatContainer.scrollHeight
 
-    // specific message div 
     const messageDiv = document.getElementById(uniqueId)
 
-    // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
     const response = await fetch('https://chatgpt-app-nt61.onrender.com', {
@@ -100,15 +89,15 @@ const handleSubmit = async (e) => {
     messageDiv.innerHTML = " "
 
     if (response.ok) {
-        const data = await response.json();
-        const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
+        const data = await response.json()
+        const parsedData = data.bot.trim()
 
         typeText(messageDiv, parsedData)
     } else {
         const err = await response.text()
 
-        messageDiv.innerHTML = "Something went wrong"
-        alert(err)
+        messageDiv.innerHTML = "Something went wrong!"
+        alert(err);
     }
 }
 
